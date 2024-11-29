2f0664dbc9a3a9e0615c323ec383d9e7bb262e73
Songsungsang <mrcomsu@gmail.com>
2024-11-29 13:51:06 +0900

    [11.29] Practicing print in Python

---
+++ Pracetice/practice.py
@@ -0,0 +1,16 @@
+#print("Hello world")
+#print("Hello", "Python")
+
+#print("Hello", 1, 2, "asdfsdf", sep="")
+#print("Hello", 1, 2, "asdfsdf", sep=" ")
+#print("Hello", 1, 2, "asdfsdf", sep="\n")
+#print("안녕하세요", end="___")
+#print("코딩온입니다.")
+
+ive = "I AM"
+print(ive)
+ive = "신뢰에요"
+print(ive)
+name = "Harin"
+name = name + " Victoria"
+print(name)
